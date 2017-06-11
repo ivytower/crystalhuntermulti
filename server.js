8c6fd6f849de36e6d5c811e2b30d0194b31b11d7
ivytower <ivytowerllc@gmail.com>
2017-06-10 23:00:58 -0400

    documentation

--- server.js
+++ server.js
@@ -2,31 +2,29 @@
  * Created by Cash on 5/14/2017.
  */
 
-var express=require('express');
-var app=express();
-var server=app.listen(3000);
-app.use(express.static('client'));
+var express=require('express');         // Require node module express
+var app=express();                      // Create an express object
+var server=app.listen(3000);            // Tell the express object to listen on port 3000
+app.use(express.static('/client'));     // Have the express object use the client directory
 
-console.log("socket server running");
-var socket=require('socket.io');
-var io=socket(server);
+console.log("socket server running");   
+var socket=require('socket.io');        // Require node module socket.io
+var io=socket(server);                  // Create a connection with the express object 
 var pack = [];
 var connections;
 logsprite();
 io.sockets.on('connection',newConnection);
 
 function newConnection(socket) {
-    connections++;
-    console.log('new connection' + socket.id);
-    socket.on('sprites', sendast);
-    socet.on('getshared', logshared);
-    
+    connections++;                                  // Increases the number on active connections
+    console.log('New Connection: ' + socket.id);    // Logs the new socket connection id
+    socket.on('sprites', sendast);                  // 
+    socket.on('getshared', logshared);              // 
 
 
 
 
-
-
+}
 function sendast() {
 
     socket.emit('getasteroids', pack);
@@ -42,7 +40,7 @@ function logsprite() {
         for (var i = 0; i < 50; i++) {
 
             pack.push({
-                key: Math.floor(Math.random() * 5),
+                key: Math.floor(Math.random() * 5),     // Picks a random color asteroid
                 x: Math.random() * (5000 - 500) + 500,
                 y: Math.random() * (5000 - 500) + 500,
             });
